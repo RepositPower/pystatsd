49aae7565d58da6993153b27807219a5af529c06
Tomas Krajca <tomas@repositpower.com>
2015-09-24 14:25:43 +1000

    Shut zbxsend up

--- pystatsd/__init__.py
+++ pystatsd/__init__.py
@@ -1,10 +1,14 @@
 try:
     from rutils import setupLogging
     setupLogging()
+
+    import logging
+    zbxsend_logger = logging.getLogger('zbxsender')
+    zbxsend_logger.setLevel(logging.CRITICAL)
 except ImportError:
     pass
 
 from .statsd import Client
 from .server import Server
 
-VERSION = (0, 1, 10)
+VERSION = (0, 1, 6)
